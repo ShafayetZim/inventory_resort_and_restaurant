dfd8edaf14fc35ef836f9748c5e98f64268ebd53
ShafayetZim <shafayet.zim@northsouth.edu>
2022-12-30 18:34:20 +0600

    customer code added

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.db.models import Sum
 from datetime import date
+from django.contrib.auth.models import User
 
 
 class Brand(models.Model):
@@ -78,3 +79,99 @@ class PurchaseProducts(models.Model):
 
     def __str__(self):
         return str(f"{self.purchase.code} - {self.product.name}")
+
+
+def customer_code():
+    prifix = "RR-"
+    next_number = '00001'
+    last_number = Customers.objects.filter(customer_code__startswith=prifix).order_by('customer_code').last()
+    if last_number:
+        last_code = int(last_number.customer_code[4:])
+        next_number = '{0:05d}'.format(last_code + 1)
+    return prifix + next_number
+
+
+class Customers(models.Model):
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    customer_code = models.CharField(max_length=20, primary_key=True, default=customer_code)
+    customer_name = models.CharField(max_length=100, blank=True, null=True)
+    customer_address = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=50, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    bin_no = models.CharField(max_length=100, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.customer_name
+
+
+class Origin(models.Model):
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    origin_name = models.CharField(max_length=50, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.origin_name
+
+
+class Category(models.Model):
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    category_name = models.CharField(max_length=100, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Packing(models.Model):
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    packing_type = models.CharField(max_length=100, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='packing_author', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Product Unit'
+        verbose_name_plural = 'Product Unit'
+
+    def __str__(self):
+        return self.packing_type
+
+
+class Product(models.Model):
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    product_name = models.CharField(max_length=100, blank=True, null=True)
+    packing = models.ForeignKey(Packing, on_delete=models.DO_NOTHING, blank=True, null=True)
+    unit = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    packing_extra = models.CharField(max_length=20, blank=True, null=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, blank=True, null=True)
+    origin = models.ForeignKey(Origin, on_delete=models.DO_NOTHING, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product_name
+
+
+class Warehouse(models.Model):
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    warehouse_name = models.CharField(max_length=100, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.warehouse_name
+
+
+
+
+
