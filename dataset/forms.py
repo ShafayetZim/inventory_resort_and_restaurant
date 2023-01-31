df412ac4e9c42cfa23f6fbaba78b626079076a9c
ShafayetZim <shafayet.zim@northsouth.edu>
2023-01-31 12:34:25 +0600

    shop, payment set added

--- dataset/forms.py
+++ dataset/forms.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from django import forms
 from dataset import models
 import datetime
-from numpy import require
 from tabnanny import check
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
@@ -126,16 +125,39 @@ class SaveProduct(forms.ModelForm):
         raise forms.ValidationError(f"Product code {product.code} already exist. Try other codes.")
 
 
+class SaveShop(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+
+    class Meta:
+        model = models.Shop
+        fields = ('name',)
+
+    def clean_name(self):
+        id = self.data['id'] if (self.data['id']).isnumeric() else 0
+        name = self.cleaned_data['name']
+        try:
+            if id > 0:
+                shop = models.Shop.objects.exclude(id=id).get(name=name)
+            else:
+                shop = models.Shop.objects.get(name=name)
+        except:
+            return name
+        raise forms.ValidationError(f"Shop name {shop.name} already exist. Try other names.")
+
+
 class SavePurchase(forms.ModelForm):
     code = forms.CharField(max_length=50)
     total_amount = forms.CharField(max_length=10)
     paid = forms.CharField(max_length=10)
     due = forms.CharField(max_length=10)
     status = forms.CharField(max_length=3)
+    shop = forms.Select(
+        attrs={'class': 'form-control form-control-sm', 'value': '', 'id': 'id_shop'}
+    )
 
     class Meta:
         model = models.PurchaseSet
-        fields = ('code', 'total_amount', 'paid', 'due', 'status',)
+        fields = ('code', 'total_amount', 'paid', 'due', 'status', 'shop',)
 
     def clean_code(self):
         code = self.cleaned_data['code']
@@ -156,6 +178,7 @@ class SavePurchase(forms.ModelForm):
     def save(self):
         instance = self.instance
         Products = []
+        Payments = []
 
         if 'product_id[]' in self.data:
             for k, val in enumerate(self.data.getlist('product_id[]')):
@@ -172,18 +195,109 @@ class SavePurchase(forms.ModelForm):
                     print(err)
                     return False
 
+        if 'pay_id[]' in self.data:
+            for k, val in enumerate(self.data.getlist('pay_id[]')):
+                pay = models.Pay.objects.get(id=val)
+                note = self.data.getlist('pay_note[]')[k]
+                price = self.data.getlist('pay_price[]')[k]
+                date = self.data.getlist('pay_date[]')[k]
+                total = float(price)
+
+                try:
+                    Payments.append(models.PurchasePayment(purchase=instance, pay=pay, note=note, amount=price, date=date, total_amount=total))
+                    print("Purchase Payments..")
+                except Exception as err:
+                    print(err)
+                    return False
+
         try:
             instance.save()
             models.PurchaseItem.objects.filter(purchase=instance).delete()
             models.PurchaseItem.objects.bulk_create(Products)
+            models.PurchasePayment.objects.filter(purchase=instance).delete()
+            models.PurchasePayment.objects.bulk_create(Payments)
         except Exception as err:
             print(err)
             return False
 
 
+class SavePayment(forms.ModelForm):
+    code = forms.CharField(max_length=50)
+    total_amount = forms.CharField(max_length=10)
+
+    class Meta:
+        model = models.PaymentSet
+        fields = ('code', 'total_amount',)
+
+    def clean_code(self):
+        code = self.cleaned_data['code']
+
+        if code == 'generate':
+            pref = datetime.datetime.now().strftime('%y%m%d')
+            code = 1
+            while True:
+                try:
+                    check = models.PaymentSet.objects.get(code=f"{pref}{code:05d}")
+                    code = code + 1
+                except:
+                    return f"{pref}{code:05d}"
+                    break
+        else:
+            return code
+
+    def save(self):
+        instance = self.instance
+        Payments = []
+
+        if 'shop_id[]' in self.data:
+            for k, val in enumerate(self.data.getlist('shop_id[]')):
+                shop = models.Shop.objects.get(id=val)
+                note = self.data.getlist('shop_note[]')[k]
+                price = self.data.getlist('shop_price[]')[k]
+                date = self.data.getlist('shop_date[]')[k]
+                total = float(price)
+
+                try:
+                    Payments.append(models.PaymentItem(payment=instance, shop=shop, note=note, amount=price, date=date, total_amount=total))
+                    print("PaymentsItems..")
+                except Exception as err:
+                    print(err)
+                    return False
+
+        try:
+            instance.save()
+            models.PaymentItem.objects.filter(payment=instance).delete()
+            models.PaymentItem.objects.bulk_create(Payments)
+        except Exception as err:
+            print(err)
+            return False
+
+
+class SaveClient(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+
+    class Meta:
+        model = models.Client
+        fields = ('name',)
+
+    def clean_name(self):
+        id = self.data['id'] if (self.data['id']).isnumeric() else 0
+        name = self.cleaned_data['name']
+        try:
+            if id > 0:
+                client = models.Client.objects.exclude(id=id).get(name=name)
+            else:
+                client = models.Client.objects.get(name=name)
+        except:
+            return name
+        raise forms.ValidationError(f"Client name {client.name} already exist. Try other names.")
+
+
 class SaveSell(forms.ModelForm):
     code = forms.CharField(max_length=50)
-    client = forms.CharField(max_length=2)
+    client = forms.Select(
+        attrs={'class': 'form-control form-control-sm', 'value': '', 'id': 'id_client'}
+    )
     status = forms.CharField(max_length=3)
     total_amount = forms.CharField(max_length=10)
     paid = forms.CharField(max_length=10)
